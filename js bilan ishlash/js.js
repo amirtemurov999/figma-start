fbe307ee1d6c158194452f93119db1b2b2b76500
Amir Temurov <amirtemurov999@gmail.com>
2022-01-23 20:27:57 +0500

    nimalardir

---
+++ js bilan ishlash/js.js
@@ -0,0 +1,5 @@
+let nima=document.getElementById("nimadir")
+nima.innerHTML="<h1>salom dunyo</h1><hr>"
+let tegma=document.getElementById("btn")
+tegma.onclick=function(){
+    nima.classList.add("body")}
