9837b8414ad337740f395a4cc4ebe54f51e49a7d
Amir Temurov <amirtemurov999@gmail.com>
2022-02-03 15:53:19 +0500

    sds

--- so'zni top o'yini/index.js
+++ so'zni top o'yini/index.js
@@ -1,14 +1,22 @@
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+let bolalar = document.querySelectorAll(".klaviatura .c");
+console.log(bolalar);
 
-// let name = prompt("Ismingiz Nima?");
-// if (!name) {
-//   while (!name) {
-//     name = prompt("Ismingdi kiriteee");
-//   }
-// }
+let matnlar = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
+matnlar.forEach((matn, index) => {
+  matn.split("").forEach((harf) => {
+    bolalar[index].innerHTML += `<span class="tugma">${harf}</span>`;
+  });
+});
 
+let tugmalar = {};
+document.querySelectorAll(".tugma").forEach((tugma) => {
+  tugmalar[tugma.innerText] = tugma;
+});
+
+console.log(tugmalar);
 let ism = document.getElementById("nom");
 ism.innerText = "Salom," + name;
 
@@ -34,7 +42,6 @@ var a = baza1[getRandomInt(baza1.length)];
 
 ism.innerHTML = baza[a];
 
-let unlilar = "aouie".split("");
 let harflar = a.split("");
 let korinadiganHarflarSoni = Math.round(harflar.length * 0.3);
 console.log(korinadiganHarflarSoni);
@@ -48,5 +55,37 @@ console.log(a.split(""));
 console.log(harflar);
 
 a.split("").forEach((harf, index) => {
-  maydon.innerHTML += `<span>${harflar[index] == "^" ? harf : "_"}</span>`;
+  let inputcha = document.createElement("input");
+  inputcha.onkeydown = klavishBosilganda;
+  inputcha.id = "harf" + index;
+  inputcha.tabIndex = index;
+  inputcha.value = harflar[index] == "^" ? harf : "";
+  inputcha.readOnly = harflar[index] == "^";
+  maydon.appendChild(inputcha);
+});
+
+function klavishBosilganda(key) {
+  let id = key.target.tabIndex;
+  if (key.target.value !== "" && key.which !== 8) {
+    key.preventDefault();
+  } else if (key.which == 8) {
+    if (id > 0) {
+      key.target.value = "";
+      document.getElementById("harf" + (id - 1)).focus();
+    }
+  }
+  key.target.value = key.key;
+  document.getElementById("harf" + (id + 1)).focus();
+}
+
+window.addEventListener("keydown", (key) => {
+  if (tugmalar[key.key]) {
+    tugmalar[key.key].classList.add("down");
+  }
+});
+
+window.addEventListener("keyup", (key) => {
+  if (tugmalar[key.key]) {
+    tugmalar[key.key].classList.remove("down");
+  }
 });
